9b994a98f11e8642de8ee0eaf89fd03368a3d953
LKMTEnjoyer <itsnotitsnotgibberish@proton.me>
2025-01-21 11:35:29 -0500

    uh

--- out/html/sandbox.js
+++ out/html/sandbox.js
@@ -1,5 +1,4 @@
 function change_var(variable, amount) {
     dendryUI.dendryEngine.state.qualities[variable] += amount;
-    dendryUI.dendryEngine.state.qualities.rb_strength_2 = dendryUI.dendryEngine.state.qualities.rb_strength.toFixed(1);
     window.updateSidebar();
 }
