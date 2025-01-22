c73e4c7beed0c6f5d3e8ddf95c69ca557545971c
LKMTEnjoyer <itsnotitsnotgibberish@proton.me>
2025-01-22 16:26:28 -0500

    add a message

--- out/html/sandbox.js
+++ out/html/sandbox.js
@@ -4,10 +4,14 @@ function change_var(variable, amount) {
 }
 
 function clear_saves() {
+    let confirmation = confirm("Do you want to delete all saves?");
+
+    if (confirmation) {
     var save_prefix = "Social Democracy: An Alternate History_Autumn Chen_save_"
     for (let slot = 0; slot <= 7; slot++) {
         localStorage[save_prefix + slot] = '';
         localStorage[save_prefix + '_timestamp_' + slot] = '';
     }
     this.populateSaveSlots(8, 2);
+    }
 }
