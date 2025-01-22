643740ffbb7f6f73bd43566caf09db28cea6d7fd
LKMTEnjoyer <itsnotitsnotgibberish@proton.me>
2025-01-22 16:20:55 -0500

    hm

--- out/html/sandbox.js
+++ out/html/sandbox.js
@@ -2,3 +2,12 @@ function change_var(variable, amount) {
     dendryUI.dendryEngine.state.qualities[variable] += amount;
     window.updateSidebar();
 }
+
+function clear_saves() {
+    var save_prefix = "Social Democracy: An Alternate History_Autumn Chen_save_"
+    for (let slot = 0; slot <= 7; slot++) {
+        localStorage[save_prefix + slot] = '';
+        localStorage[save_prefix + '_timestamp_' + slot] = '';
+    }
+    this.populateSaveSlots(8, 2);
+}
